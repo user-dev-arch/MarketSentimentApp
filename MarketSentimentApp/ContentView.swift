875461bbdbd36a6cc02c112f65cc070ebce8465a
Madaminov Muhammadjon <90606773+Madaminov0888@users.noreply.github.com>
2025-12-04 11:27:47 +0500

    [fea] tab view added

--- MarketSentimentApp/ContentView.swift
+++ MarketSentimentApp/ContentView.swift
@@ -7,16 +7,83 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+enum TabSelections {
+    case dashboard, news, watchlist, alerts
+    
+    
+    var title: String {
+        switch self {
+        case .dashboard:
+            return "Dashboard"
+        case .news:
+            return "News"
+        case .watchlist:
+            return "Watchlist"
+        case .alerts:
+            return "Alerts"
         }
-        .padding()
     }
+    
+    var icon: String {
+        switch self {
+        case .dashboard:
+            return "house.fill"
+        case .news:
+            return "newspaper"
+        case .watchlist:
+            return "star"
+        case .alerts:
+            return "bell.and.waves.left.and.right.fill"
+        }
+    }
+}
+
+
+struct ContentView: View {
+    @State private var tabSelection: TabSelections = .dashboard
+    
+    var body: some View {
+        TabView(selection: $tabSelection) {
+            tabs
+        }
+        .tabViewStyle(.automatic)
+    }
+    
+    
+    @TabContentBuilder<TabSelections>
+    private var tabs: some TabContent<TabSelections> {
+        dashboardTab
+        newsTab
+        WatchlistsTab
+        alertsTab
+    }
+    
+    
+    private var dashboardTab: some TabContent<TabSelections> {
+        Tab(TabSelections.dashboard.title, image: TabSelections.dashboard.icon, value: .dashboard) {
+            DashboardView()
+        }
+    }
+    
+    
+    private var newsTab: some TabContent<TabSelections> {
+        Tab(TabSelections.news.title, image: TabSelections.news.icon, value: .news) {
+            NewsView()
+        }
+    }
+    
+    private var WatchlistsTab: some TabContent<TabSelections> {
+        Tab(TabSelections.watchlist.title, image: TabSelections.watchlist.icon, value: .watchlist) {
+            WatchlistsView()
+        }
+    }
+    
+    private var alertsTab: some TabContent<TabSelections> {
+        Tab(TabSelections.alerts.title, image: TabSelections.alerts.icon, value: .alerts) {
+            AlertsView()
+        }
+    }
+    
 }
 
 #Preview {
---
+++ MarketSentimentApp/Core/DashboardView.swift
@@ -0,0 +1,18 @@
+//
+//  DashboardView.swift
+//  MarketSentimentApp
+//
+//  Created by Muhammadjon Madaminov on 04/12/25.
+//
+
+import SwiftUI
+
+struct DashboardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DashboardView()
+}
